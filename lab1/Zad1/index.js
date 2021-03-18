b432fcca18e1e408e920658e3025111c363a93e2
Michał Szczepanowski gr 3 <56340908+mszczepanowski11@users.noreply.github.com>
2021-03-18 20:45:11 +0100

    Delete index.ts

--- lab1/Zad1/index.js
+++ lab1/Zad1/index.js
@@ -1,2 +1,16 @@
-var personName = 'John';
-document.body.innerHTML = "<h1>Hello " + personName + "</h1>";
+// const personName = 'John';
+// document.body.innerHTML = `<h1>Hello ${personName}</h1>`
+var Person = (function () {
+    function Person(name, surname, age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+    return Person;
+})();
+var p = new Person("John", "Blake", 11);
+const Show = () => {
+    return "Witaj " + p.name + " " + p.surname + ", mam " + p.age + " lat";
+}
+Show();
+document.getElementById('header').innerHTML = Show();
--- lab1/Zad1/index.ts
+++ lab1/Zad1/index.ts
@@ -1,2 +1,21 @@
-const personName = 'John';
-document.body.innerHTML = `<h1>Hello ${personName}</h1>`
+// const personName = 'John';
+// document.body.innerHTML = `<h1>Hello ${personName}</h1>`
+
+class Person{
+    name:string;
+    surname:string;
+    age:number;
+
+    constructor(name:string, surname:string, age:number){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+}
+
+let p = new Person("John","Blake",11);
+
+const Show = () => {
+    console.log(`Witaj ${p.name} ${p.surname}  ", mam "  ${p.age} + " lat"`);
+}
+Show();
