cb308bab4451ef93cced8daea669101758be8d50
Michał Szczepanowski gr 3 <m.szczepanowski11@gmail.com>
2021-06-11 10:46:58 +0200

    Added game logic

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -1,28 +1,125 @@
+"use strict";
+exports.__esModule = true;
+exports.Board = exports.Cell = void 0;
+var currentPlayer = 'X';
+var gameRunning = true;
 var Cell = /** @class */ (function () {
     function Cell(cell) {
         this.htmlELement = cell;
+        this.cellValue = 0;
     }
+    Cell.prototype.setCellValue = function (value) {
+        var PlayerX = new Image(50);
+        var PlayerO = new Image(50);
+        PlayerX.src = 'x_sign.png';
+        PlayerO.src = 'o_sign.png';
+        PlayerX.classList.add('img');
+        PlayerO.classList.add('img');
+        if (value == 1 && this.cellValue == 0) {
+            this.htmlELement.appendChild(PlayerX);
+        }
+        else if (value == -1 && this.cellValue == 0) {
+            this.htmlELement.appendChild(PlayerO);
+        }
+    };
     return Cell;
 }());
+exports.Cell = Cell;
 var Board = /** @class */ (function () {
     function Board(size) {
+        var _this = this;
         this.cells = new Array(size);
+        this.currentSymbol = 1;
         var table = document.getElementById("tictactoe");
         var i = 0;
         for (var r = 0; r < size; r++) {
             var row = table.insertRow(r);
-            for (var c = 0; c < size; c++) {
+            var _loop_1 = function (c) {
                 var cell = row.insertCell(c);
                 cell.className = 'cell';
                 var newCell = new Cell(cell);
-                this.cells[i] = newCell;
-                cell.addEventListener("click", function () {
-                    cell.innerHTML = "X";
-                });
+                this_1.cells[i] = newCell;
+                gameRunning ?
+                    cell.addEventListener("click", function () {
+                        return _this.makeMove(newCell);
+                    }, true)
+                    :
+                        cell.removeEventListener("click", function () {
+                            return _this.makeMove(newCell);
+                        }, false);
+                i++;
+            };
+            var this_1 = this;
+            for (var c = 0; c < size; c++) {
+                _loop_1(c);
             }
         }
     }
+    Board.prototype.boardIsFull = function () {
+        for (var i = 0; i < this.cells.length; i++) {
+            if (this.cells[i].cellValue == 0)
+                return false;
+        }
+        return true;
+    };
+    Board.prototype.makeMove = function (cell) {
+        currentPlayer = this.currentSymbol === 1 ? 'X' : 'O';
+        var statusDisplay = document.querySelector('.game-status');
+        cell.setCellValue(this.currentSymbol);
+        if (cell.cellValue == 0) {
+            if (this.currentSymbol == 1) {
+                cell.cellValue = -1;
+                this.currentSymbol = -1;
+                statusDisplay.innerHTML = 'Turn:O';
+            }
+            else if (this.currentSymbol == -1) {
+                cell.cellValue = 1;
+                this.currentSymbol = 1;
+                statusDisplay.innerHTML = 'Turn:X';
+            }
+        }
+        //DRAW
+        if (this.boardIsFull()) {
+            console.log(this.boardIsFull());
+            statusDisplay.innerHTML = 'Draw!';
+        }
+        //HORIZONTALLY
+        if (this.cells[0].cellValue != 0 && this.cells[0].cellValue == this.cells[1].cellValue && this.cells[1].cellValue == this.cells[2].cellValue) {
+            statusDisplay.innerHTML = currentPlayer + " has won!";
+            gameRunning = false;
+        }
+        if (this.cells[3].cellValue != 0 && this.cells[3].cellValue == this.cells[4].cellValue && this.cells[4].cellValue == this.cells[5].cellValue) {
+            statusDisplay.innerHTML = currentPlayer + " has won!";
+            gameRunning = false;
+        }
+        if (this.cells[6].cellValue != 0 && this.cells[6].cellValue == this.cells[7].cellValue && this.cells[7].cellValue == this.cells[8].cellValue) {
+            statusDisplay.innerHTML = currentPlayer + " has won!";
+            gameRunning = false;
+        }
+        //VERTICALLY
+        if (this.cells[0].cellValue != 0 && this.cells[0].cellValue == this.cells[3].cellValue && this.cells[3].cellValue == this.cells[6].cellValue) {
+            statusDisplay.innerHTML = currentPlayer + " has won!";
+            gameRunning = false;
+        }
+        if (this.cells[1].cellValue != 0 && this.cells[1].cellValue == this.cells[4].cellValue && this.cells[4].cellValue == this.cells[7].cellValue) {
+            statusDisplay.innerHTML = currentPlayer + " has won!";
+            gameRunning = false;
+        }
+        if (this.cells[2].cellValue != 0 && this.cells[2].cellValue == this.cells[5].cellValue && this.cells[5].cellValue == this.cells[8].cellValue) {
+            statusDisplay.innerHTML = currentPlayer + " has won!";
+            gameRunning = false;
+        }
+        //DIAGONALLY
+        if (this.cells[0].cellValue != 0 && this.cells[0].cellValue == this.cells[4].cellValue && this.cells[4].cellValue == this.cells[8].cellValue) {
+            statusDisplay.innerHTML = currentPlayer + " has won!";
+            gameRunning = false;
+        }
+        if (this.cells[2].cellValue != 0 && this.cells[2].cellValue == this.cells[4].cellValue && this.cells[4].cellValue == this.cells[6].cellValue) {
+            statusDisplay.innerHTML = currentPlayer + " has won!";
+            gameRunning = false;
+        }
+    };
     return Board;
 }());
-
-const t = new Board(3)
+exports.Board = Board;
+var board = new Board(3);
--- TicTacToe/index.ts
+++ TicTacToe/index.ts
@@ -1,29 +1,135 @@
-    class Cell {
-        htmlELement:HTMLElement
-        constructor(cell:HTMLElement){
-           this.htmlELement = cell
-        }
+let currentPlayer = 'X'
+let gameRunning: boolean = true
+export class Cell {
+   cellValue: number
+   htmlELement: HTMLElement
+
+   constructor(cell: HTMLElement) {
+      this.htmlELement = cell
+      this.cellValue = 0
    }
 
-   class Board {
-        cells : Cell[]
+   setCellValue(value: number) {
+      const PlayerX: HTMLImageElement = new Image(50)
+      const PlayerO: HTMLImageElement = new Image(50)
 
-        constructor(size:number){
-            this.cells = new Array(size)
+      PlayerX.src = 'x_sign.png'
+      PlayerO.src = 'o_sign.png'
 
-            let table = <HTMLTableElement>document.getElementById("tictactoe")
-            let i = 0;
-            for(let r= 0; r < size; r++){
-                let row = table.insertRow(r)
-                for(let c = 0; c < size; c++){
-                    let cell = <HTMLTableDataCellElement>row.insertCell(c)
-                    cell.className = 'cell'
-                    const newCell = new Cell(cell)
-                    this.cells[i] = newCell
-                    cell.addEventListener("click", () => {
-                        console.log("cell was clicked")
-                    })
-                }
-            }
-        }
+      PlayerX.classList.add('img')
+      PlayerO.classList.add('img')
+
+      if (value == 1 && this.cellValue == 0) {
+         this.htmlELement.appendChild(PlayerX)
+      } else if (value == -1 && this.cellValue == 0) {
+         this.htmlELement.appendChild(PlayerO)
+      }
    }
+}
+
+export class Board {
+   cells: Cell[]
+   currentSymbol: number
+
+   constructor(size: number) {
+      this.cells = new Array(size)
+      this.currentSymbol = 1
+
+      let table = <HTMLTableElement>document.getElementById("tictactoe")
+      let i = 0;
+      for (let r = 0; r < size; r++) {
+         let row = table.insertRow(r)
+         for (let c = 0; c < size; c++) {
+            let cell = <HTMLTableDataCellElement>row.insertCell(c)
+            cell.className = 'cell'
+            const newCell = new Cell(cell)
+            this.cells[i] = newCell
+
+            gameRunning ?
+               cell.addEventListener("click", () =>
+                  this.makeMove(newCell), true)
+               :
+               cell.removeEventListener("click", () =>
+                  this.makeMove(newCell), false)
+            i++
+         }
+      }
+   }
+
+   boardIsFull():boolean{
+      for(let i = 0; i < this.cells.length;i++){
+         if (this.cells[i].cellValue == 0)
+            return false
+      }
+      return true
+   }
+
+   makeMove(cell: Cell): void {
+
+      currentPlayer = this.currentSymbol === 1 ? 'X' : 'O'
+      const statusDisplay = <HTMLElement>document.querySelector('.game-status')
+      cell.setCellValue(this.currentSymbol)
+
+      if (cell.cellValue == 0) {
+         if (this.currentSymbol == 1) {
+            cell.cellValue = -1
+            this.currentSymbol = -1
+            statusDisplay.innerHTML = 'Turn:O'
+
+         } else if (this.currentSymbol == -1) {
+            cell.cellValue = 1
+            this.currentSymbol = 1
+            statusDisplay.innerHTML = 'Turn:X'
+         }
+      }
+
+      //DRAW
+      if (this.boardIsFull()) {
+         console.log(this.boardIsFull())
+         statusDisplay.innerHTML = 'Draw!';
+     }
+
+      //HORIZONTALLY
+      if (this.cells[0].cellValue != 0 && this.cells[0].cellValue == this.cells[1].cellValue && this.cells[1].cellValue == this.cells[2].cellValue) {
+         statusDisplay.innerHTML = `${currentPlayer} has won!`
+         gameRunning = false
+      }
+      if (this.cells[3].cellValue != 0 && this.cells[3].cellValue == this.cells[4].cellValue && this.cells[4].cellValue == this.cells[5].cellValue) {
+         statusDisplay.innerHTML = `${currentPlayer} has won!`
+         gameRunning = false
+      }
+      if (this.cells[6].cellValue != 0 && this.cells[6].cellValue == this.cells[7].cellValue && this.cells[7].cellValue == this.cells[8].cellValue) {
+         statusDisplay.innerHTML = `${currentPlayer} has won!`
+         gameRunning = false
+      }
+
+
+      //VERTICALLY
+      if (this.cells[0].cellValue != 0 && this.cells[0].cellValue == this.cells[3].cellValue && this.cells[3].cellValue == this.cells[6].cellValue) {
+         statusDisplay.innerHTML = `${currentPlayer} has won!`
+         gameRunning = false
+      }
+      if (this.cells[1].cellValue != 0 && this.cells[1].cellValue == this.cells[4].cellValue && this.cells[4].cellValue == this.cells[7].cellValue) {
+         statusDisplay.innerHTML = `${currentPlayer} has won!`
+         gameRunning = false
+      }
+      if (this.cells[2].cellValue != 0 && this.cells[2].cellValue == this.cells[5].cellValue && this.cells[5].cellValue == this.cells[8].cellValue) {
+         statusDisplay.innerHTML = `${currentPlayer} has won!`
+         gameRunning = false
+      }
+
+
+      //DIAGONALLY
+      if (this.cells[0].cellValue != 0 && this.cells[0].cellValue == this.cells[4].cellValue && this.cells[4].cellValue == this.cells[8].cellValue) {
+         statusDisplay.innerHTML = `${currentPlayer} has won!`
+         gameRunning = false
+      }
+      if (this.cells[2].cellValue != 0 && this.cells[2].cellValue == this.cells[4].cellValue && this.cells[4].cellValue == this.cells[6].cellValue) {
+         statusDisplay.innerHTML = `${currentPlayer} has won!`
+         gameRunning = false
+      }
+      
+   }
+}
+
+const board = new Board(3)
