54a5ad2a96dec08978211277b736434b9c602c79
Michał Szczepanowski gr 3 <56340908+mszczepanowski11@users.noreply.github.com>
2021-06-11 10:42:49 +0200

    Delete Factory directory

--- Factory/games.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Games = void 0;
-var Games;
-(function (Games) {
-    Games[Games["TicTacToe"] = 1] = "TicTacToe";
-    Games[Games["BattleShips"] = 2] = "BattleShips";
-})(Games = exports.Games || (exports.Games = {}));
--- Factory/games.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum Games {
-    TicTacToe = 1,
-    BattleShips = 2
-}
